d7e3e54ae5d25659d6958d911f31e45be9a7758c
Priyanshu Goyal <priyanshu327goyal@gmail.com>
2023-07-04 15:16:35 +0530

    add home page

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 main().catch(err=>console.log(err));
 async function main(){
-    await mongoose.connect("mongodb+srv://priyanshu327goyal:"+ process.env.PASSWORD+"@lambda.5se61ae.mongodb.net/lambdaDB",{ useNewUrlParser:true});
+    await mongoose.connect("mongodb://127.0.0.1:27017/lambdaDB",{ useNewUrlParser:true});
 
     const itemsRouter=require('./routes/items');
     const blogsRouter=require('./routes/blogs');
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,7 +6,8 @@ function Home() {
         <div class=" home">
             <div class="profile-img"><img src="./20230506_175427.jpg" /></div>
             <div class="about-me">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis malesuada rutrum velit et lobortis. Donec eget ullamcorper magna. Mauris iaculis, neque eget tempor pretium, eros lacus gravida tortor, ac efficitur velit leo id justo. Quisque viverra faucibus orci id volutpat. Etiam id sagittis dui. Vestibulum suscipit in ligula mattis porttitor. Phasellus urna sapien, iaculis ut erat vel, sollicitudin commodo libero. Pellentesque lacinia, nisl ac commodo sollicitudin, nisi neque suscipit tortor, sit amet luctus diam sem a felis. Nam sodales augue eget laoreet iaculis. Praesent dapibus est sed auctor tincidunt. Sed sodales risus eros, eget suscipit elit varius et. Maecenas tempus semper condimentum. Quisque ornare, nunc ut molestie mattis, neque nulla lacinia leo, congue commodo enim neque in est. Proin urna ante, suscipit vel ex a, ultrices commodo eros. Cras molestie turpis vitae odio dictum facilisis. Sed maximus, eros vitae efficitur lobortis, metus odio laoreet dui, a posuere lacus ante et nisi.</p>
+            <p>Hey guys! My name is Priyanshu Goyal. I am a first year undergrad pursuing BTech in Maths & Computing at IIT Hyderabad. I am a learning programmer and web developer. I give coding contests on platforms like codechef and codeforces to improve my problem solving and logical reasoning skills. I am looking for a project in web development to gain some practice and hands on experience in real world applications.
+            I made this website as my project for lambda. This website is made using React for frontend and express for backend. The database for the website is hosted on MongoDb atlas.</p>
             </div>
         </div>
     )
