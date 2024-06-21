a07fb8bc7e7a18e55ff9062640e8de24a91bedcf
Priyanshu327Goyal <130755283+Priyanshu327Goyal@users.noreply.github.com>
2024-06-21 12:22:43 +0530

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,8 +6,8 @@ function Home() {
         <div class=" home">
             <div class="profile-img"><img src="./20230506_175427.jpg" /></div>
             <div class="about-me">
-            <p>Hey guys! My name is Priyanshu Goyal. I am a first year undergrad pursuing BTech in Maths & Computing at IIT Hyderabad. I am a learning programmer and web developer. I give coding contests on platforms like codechef and codeforces to improve my problem solving and logical reasoning skills. I am looking for a project in web development to gain some practice and hands on experience in real world applications.
-            I made this website as my project for lambda. This website is made using React for frontend and express for backend. The database for the website is hosted on MongoDb atlas.</p>
+            <p>Hey guys! My name is Priyanshu Goyal. I am a third year undergrad pursuing BTech in Maths & Computing at IIT Hyderabad. I am a competitive programmer and web developer. I give coding contests on codeforces and solve questions of leetcode to improve my problem solving and logical reasoning skills. I am looking for a project in web development to gain some practice and hands on experience in real world applications. 
+                This website is made using React for frontend and express for backend. The database for the website is hosted on MongoDb atlas.</p>
             </div>
         </div>
     )
